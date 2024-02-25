91189158753d2412b96040402fd43500925ea209
Kaan Adiyaman <152101574+kaanthecoder@users.noreply.github.com>
2024-02-25 17:18:47 +0000

    Add files via upload

---
+++ Byb5-Capstone.py
@@ -0,0 +1,373 @@
+Here are the
+steps
+to
+accomplish
+the
+tasks:
+
+1.
+Create
+another
+GitHub
+repository
+named
+"finalCapstone".
+
+2.
+Push
+the
+last
+Capstone
+Project
+to
+the
+"finalCapstone"
+repository:
+
+- Navigate
+to
+the
+directory
+of
+your
+last
+Capstone
+Project in your
+terminal or command
+prompt.
+- Initialize
+a
+new
+Git
+repository if it
+'s not already initialized: `git init`
+- Add
+your
+remote
+repository: `git
+remote
+add
+origin[finalCapstone_repository_url]
+`
+- Push
+your
+local
+repository
+to
+the
+remote
+repository: `git
+push - u
+origin
+master
+`
+
+3.
+Add
+a
+detailed
+README
+file
+for each project:
+
+    - Create
+    a
+    README.md
+    file in the
+    root
+    directory
+    of
+    your
+    project.
+    - Edit
+    the
+    README.md
+    file and include
+    the
+    following
+    sections:
+
+    ```
+    # Project Name
+
+    ## Description
+
+    [Clear, short, and to
+    the
+    point
+    description
+    of
+    your
+    project.Describe
+    the
+    importance
+    of
+    your
+    project, and what
+    it
+    does.]
+
+    ## Table of Contents
+
+    - [Installation](  # installation)
+        - [Usage](  # usage)
+            - [Credits](  # credits)
+
+                ## Installation
+
+                [Instructions on how to install your project locally]
+
+                    ## Usage
+
+                [Instructions
+    on
+    how
+    to
+    use
+    your
+    project
+    after
+    installation.You
+    can
+    include
+    screenshots
+    here
+    to
+    demonstrate
+    your
+    project in action.]
+
+    ## Credits
+
+    [Highlight and link to the authors of your project if there are more than one.]
+    ```
+
+    4.
+    Save
+    the
+    changes
+    to
+    your
+    README.md
+    file.
+
+    5.
+    Commit
+    your
+    changes: `git
+    commit - am
+    "Add detailed README"
+    `
+
+    6.
+    Push
+    the
+    changes
+    to
+    your
+    GitHub
+    repository: `git
+    push
+    origin
+    master
+    `
+
+    Replace `[finalCapstone_repository_url]
+    `
+    with the URL of your "finalCapstone" GitHub repository.
+
+    Following these steps will create a new GitHub repository, push your last Capstone Project to it, and add detailed README files for each project.Let me know if you need further assistance!
+
+    Sure! Here are the commands you need to execute in your terminal or command prompt:
+
+        1.
+    Create
+    another
+    GitHub
+    repository
+    named
+    "finalCapstone":
+
+    curl - u
+    'your_username'
+    https: // api.github.com / user / repos - d
+    '{"name":"finalCapstone"}'
+
+    Replace
+    `'your_username'`
+    with your GitHub username.
+
+    2. Push the last Capstone Project to the "finalCapstone" repository:
+
+        Navigate
+    to
+    the
+    directory
+    of
+    your
+    last
+    Capstone
+    Project in your
+    terminal or command
+    prompt.
+
+    cd
+    path / to / lastCapstoneProject
+
+    If
+    the
+    directory is not already
+    a
+    Git
+    repository, initialize
+    a
+    new
+    Git
+    repository:
+
+    git
+    init
+
+    Add
+    your
+    remote
+    repository:
+
+    git
+    remote
+    add
+    origin
+    https: // github.com / your_username / finalCapstone.git
+
+    Push
+    your
+    local
+    repository
+    to
+    the
+    remote
+    repository:
+
+    git
+    push - u
+    origin
+    master
+
+    3.
+    Add
+    a
+    detailed
+    README
+    file
+    for each project:
+        Create
+    a
+    README.md
+    file in the
+    root
+    directory
+    of
+    your
+    project.
+
+    touch
+    README.md
+
+    Edit
+    the
+    README.md
+    file and add
+    the
+    necessary
+    content as described in the
+    previous
+    message.
+
+        # Project Name
+
+        ## Description
+
+    [Clear, short, and to
+    the
+    point
+    description
+    of
+    your
+    project.Describe
+    the
+    importance
+    of
+    your
+    project, and what
+    it
+    does.]
+
+    ## Table of Contents
+
+    - [Installation](  # installation)
+        - [Usage](  # usage)
+            - [Credits](  # credits)
+
+                ## Installation
+
+                [Instructions on how to install your project locally]
+
+                    ## Usage
+
+                [Instructions
+    on
+    how
+    to
+    use
+    your
+    project
+    after
+    installation.You
+    can
+    include
+    screenshots
+    here
+    to
+    demonstrate
+    your
+    project in action.]
+
+    ## Credits
+
+    [Highlight and link to the authors of your project if there are more than one.]
+
+    4.
+    Save
+    the
+    changes
+    to
+    your
+    README.md
+    file.
+
+    5.
+    Commit
+    your
+    changes:
+
+    git
+    add
+    README.md
+    git
+    commit - m
+    "Add detailed README
+
+    6.
+    Push
+    the
+    changes
+    to
+    your
+    GitHub
+    repository:
+
+    git
+    push
+    origin
+    master
+
